efd23b3752a0ad4a902aac56042eb34ee150a509
mohamadWatte <m.watad90@gmail.com>
2026-03-14 13:01:53 +0200

    .

---
+++ WhileDemo/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhileDemo
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //for(int i=1;i<=10;i++)
+            //    Console.Write('*');
+
+            int i = 1;
+
+            while (i<=10)
+            {
+                Console.Write('*');
+                i++;
+            }
+
+
+
+            //------2-----//
+
+            int j = 1;
+            while (j == 10)
+            {
+                Console.WriteLine(j);
+
+            }
+
+            Console.WriteLine("End Program");
+
+            Console.ReadKey();
+        }
+    }
+}
